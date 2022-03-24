fc2dc34ddd5d8ea27e0ffb0616fe6ce918407223
Elsie Fernandez <els.fernandez@icloud.com>
2022-03-23 21:00:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards (array, event) {
+    let Thanks = [];
+    for (let i = 0; i < array.length; i++) {
+Thanks.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+
+
+}
+
+return Thanks;
+}
+function countDown(){
+    let counter = 10;
+    while (counter >= 0){
+    console.log(counter)
+    counter = counter - 1
+    }
+    return counter
+
+
+}
+countDown()
